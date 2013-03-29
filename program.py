30779f69e6972bd5aabfc5fdccf1d6761dd999ba
Timothy <jazzhepcat@gmail.com>
2013-03-29 13:09:03 +1300

    I think I fixed it...

--- program.py
+++ program.py
@@ -1,303 +1,275 @@
+from math import pi, sin, cos
+from array import *
+
+
 from direct.showbase.ShowBase import ShowBase
-from direct.gui.OnscreenImage import OnscreenImage
-from direct.gui.OnscreenText import OnscreenText
-from panda3d.core import TransparencyAttrib
-from panda3d.core import TextNode
 from direct.task import Task
-from direct.gui.DirectGui import DirectFrame
-import os
-import sys
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from direct.showbase import DirectObject
+from panda3d.core import Point3
+from direct.gui.OnscreenText import OnscreenText
+from direct.gui.OnscreenImage import OnscreenImage
+import math
+
+
+# for cam control
+from pandac.PandaModules import *
 
 class MyApp(ShowBase):
-    gameMode = "Intro"
 
-    def __init__(self):
-        ShowBase.__init__(self)
-
-        #self.cameraSetup()
-        self.setupKeyboard()
-
-        self.initIntro()
-
-    def gameChangeMode(self):
-        if base.gameMode == "Intro":
-            self.initIntro()
-        elif base.gameMode == "Convo":
-            self.initConvo()
-
-    def initIntro(self):
-        self.fadeAlpha = 0.0
-        self.fadeTime = 30.0
-        self.fadeInc = 1.0/self.fadeTime
-        self.fadeDir = "up"
-
-        self.sceneImage = OnscreenImage(image = "images/scene.png", scale = (1920.0/1080.0,1,1))
-        self.sceneImage.setTransparency(TransparencyAttrib.MAlpha)
-        self.sceneImage.setAlphaScale(self.fadeAlpha)
-
-        self.imageNumber = 0
-        self.sceneImageList = ["images/scene.png","images/image1.jpg"]
-
-        #self.texty = OnscreenText(str(self.fadeAlpha))
-
-        taskMgr.add(self.gameIntroUpdate, "GameIntroUpdate")
-
-    def gameIntroUpdate(self, task):
-        self.slideManager()
-        if (base.gameMode != "Intro"):
-            self.gameChangeMode()
-            return Task.done
-
-        return Task.cont
-
-    def cameraSetup(self):
-        base.camLens.setAspectRatio(1920.0/1080.0)
-
-    def slideManager(self):
-        if self.fadeDir == "up" and self.fadeAlpha<1:
-            self.fadeAlpha += self.fadeInc
-            self.sceneImage.setAlphaScale(self.fadeAlpha)
-            #self.texty.setText(str(self.fadeAlpha))
-
-        if self.fadeDir == "down" and self.fadeAlpha > 0:
-            self.fadeAlpha -= self.fadeInc
-            self.sceneImage.setAlphaScale(self.fadeAlpha)
-
-        if self.fadeDir == "up" and self.fadeAlpha >= 1:
-            self.fadeDir = "down"
-
-        if self.fadeDir == "down" and self.fadeAlpha <= 0:
-            if self.imageNumber < 1:
-                self.fadeDir = "up"
-                self.sceneImage.setImage(self.nextImage())
-                self.sceneImage.setTransparency(TransparencyAttrib.MAlpha)
-                self.sceneImage.setAlphaScale(self.fadeAlpha)
-            else:
-                self.fadeDir = "up"
-                base.gameMode = "Convo"
-                self.sceneImage.setImage(self.sceneImageList[self.imageNumber])
-                self.sceneImage.setTransparency(TransparencyAttrib.MAlpha)
-                self.sceneImage.setAlphaScale(self.fadeAlpha)
-
-    def nextImage(self):
-            self.imageNumber += 1
-            return self.sceneImageList[self.imageNumber]
-
-    def initConvo(self):
-        self.textToType = "This will be used for talking\nThis is a good conversation box"
-        self.textVisible = ""
-
-        self.strOptionA = "Yes"
-        self.strOptionB = "No"
-        self.strOptionC = "Maybe"
-
-        self.convoResponseSelected = False
-        self.convoDepth = "1"
-
-        self.intOptionCount = 3
-
-        #self.txtConvo = OnscreenText(self.textVisible, align = TextNode.ALeft)
-        self.txtConvo = TextNode("Texty Bastard")
-        self.txtConvo.setText(self.textToType)
-        self.txtReply = TextNode("reply")
-        self.txtReply.setText("")
-        self.intHover = 0
-
-        #self.txtConvo.setFrameColor(0, 0, 1, 1)
-        #self.txtConvo.setFrameAsMargin(0.2, 0.2, 0.1, 0.1)
+	def __init__(self):
+		ShowBase.__init__(self)
 
 
 
-        myFrame = DirectFrame(frameColor=(0.8, 0.8, 0.8, 0.4),
-                      frameSize=(-1, 0, -1, 0))
-        nodePathConvo = aspect2d.attachNewNode(self.txtConvo)
-        nodePathConvo.setScale(0.07)
-        nodePathConvo.setPos(-1, 0, -0.1)
-        self.i = 0
-        self.indent = 0.0
-        self.selectedResponse = 0
-        self.txtOptionA = OnscreenText(text = "", pos = (-1,-0.6), align = TextNode.ALeft)
-        self.txtOptionB = OnscreenText(text = "", pos = (-1,-0.7), align = TextNode.ALeft)
-        self.txtOptionC = OnscreenText(text = "", pos = (-1,-0.8), align = TextNode.ALeft)
+		self.txtConvo = OnscreenText("Jim Bob:",style=1, align = TextNode.ALeft, fg=(1,1,1,1), pos = (-1.1,-0.6,0), scale = 0.1)
+		self.txtConvoOp1 = OnscreenText("  - ",style=1, align = TextNode.ALeft, fg=(1,1,1,1), pos = (-1.1,-0.65,0), scale = 0.1)
+		self.txtConvoOp2 = OnscreenText("  - ",style=1, align = TextNode.ALeft, fg=(1,1,1,1), pos = (-1.1,-0.7,0), scale = 0.1)
+		self.txtConvoOp3 = OnscreenText("  - ",style=1, align = TextNode.ALeft,fg=(1,1,1,1), pos = (-1.1,-0.75,0), scale = 0.1)
+		self.convoLineSelected = 0
 
-        taskMgr.add(self.gameConvoUpdate, "GameConvoUpdate")
+		self.keyboardSetup()
 
+		#self.cTrav=CollisionTraverser()
+		#self.collisionHandler = CollisionHandlerQueue()
 
-    def gameConvoUpdate(self, task):
-
-        if (len(self.textVisible) != len(self.textToType)):
-			task.delayTime = 0.001
-			while (self.i < len(self.textToType)):
-
-				self.textVisible += self.textToType[self.i]
-				self.txtConvo.setText(self.textVisible)
-				self.i += 1
-
-				return Task.again
-
-        else:
-            taskMgr.add(self.gameConvoOptions, "ConvoOptions")
-            return Task.done
-            #else if self.selectedResponse == 1:
-
-    def gameConvoOptions(self, task):
-        if self.convoResponseSelected == True:
-            if self.convoCheckBranch("01", self.convoDepth):
-                self.convoNextDialogue("01", self.convoDepth)
-            else:
-                self.txtConvo.setText("IT RETURNED "+str(self.branchFound))
-
-            return Task.done
-
-        elif self.selectedResponse == 0:
-            self.txtOptionA.setText(self.strOptionA)
-            self.txtOptionA.setX(-1+self.indent)
-            self.txtOptionA.setFg(fg = (1,0,0,1))
-            self.txtOptionB.setFg(fg = (0,0,0,1))
-            self.txtOptionB.setText(self.strOptionB)
-            self.txtOptionB.setX(-1)
-            self.txtOptionC.setFg(fg = (0,0,0,1))
-            self.txtOptionC.setText(self.strOptionC)
-            self.txtOptionC.setX(-1)
-            self.indent = self.getIndent(self.indent,0.01,0.1)
-            return Task.again
-        elif self.selectedResponse == 1:
-            self.txtOptionA.setFg(fg = (0,0,0,1))
-            self.txtOptionA.setText(self.strOptionA)
-            self.txtOptionA.setX(-1)
-            self.txtOptionB.setFg(fg = (1,0,0,1))
-            self.txtOptionB.setText(self.strOptionB)
-            self.txtOptionB.setX(-1+self.indent)
-            self.txtOptionC.setFg(fg = (0,0,0,1))
-            self.txtOptionC.setText(self.strOptionC)
-            self.txtOptionC.setX(-1)
-            self.indent = self.getIndent(self.indent,0.01,0.1)
-            return Task.again
-        elif self.selectedResponse == 2:
-            self.txtOptionA.setFg(fg = (0,0,0,1))
-            self.txtOptionA.setText(self.strOptionA)
-            self.txtOptionA.setX(-1)
-            self.txtOptionB.setFg(fg = (0,0,0,1))
-            self.txtOptionB.setText(self.strOptionB)
-            self.txtOptionB.setX(-1)
-            self.txtOptionC.setFg(fg = (1,0,0,1))
-            self.txtOptionC.setText(self.strOptionC)
-            self.txtOptionC.setX(-1+self.indent)
-            self.indent = self.getIndent(self.indent,0.01,0.1)
-            return Task.again
-
-    def convoGetStrings(self,id,depth):
-        path = os.path.abspath(os.getcwd())
-        f = open(path+"\\text\\stringsConvo.txt","r")
-        char = ""
-        line = ""
-
-        while line != "("+str(id)+")."+depth+":":
-            while char != ":":
-                char = f.readline(1)
-                line += char
-            if line != "("+str(id)+")."+depth+":":
-                char = ""
-                line = ""
-                f.readline()
-
-        f.readline(1)
-        line = f.readline()
-        line = line.replace("##", "\n")
-        f.close()
-        return line
-
-    def convoCheckBranch(self,id,depth):
-        path = os.path.abspath(os.getcwd())
-        f = open(path+"\\text\\stringsConvo.txt","r")
-        char = ""
-        line = ""
-        self.branchFound = False
-        counter = 0
-
-        while line != "<END>:":
-            char = ""
-            line = ""
-            while char != ":":
-                char = f.readline(1)
-                line += char
-            #self.branchFound = 3
-            if line == "<END>:" and self.branchFound == False:
-                self.branchFound = False
-            elif line == "("+str(id)+")."+depth+":":
-                self.branchFound = True
-            else:
-                f.readline()
-
-        f.close()
-        if self.branchFound == True:
-            return True
-        else:
-            return False
-
-    def convoNextDialogue(self,id,depth):
-        #id = "01"
-        #self.convoDepth = "1"
+		self.cameraDistance = -50
+		self.camAngle = -15
 
 
-        self.textToType = self.convoGetStrings(id, depth)
-        self.intOptionCount = int(self.convoGetStrings(id, depth+".OpNo"))
-        self.strOptionA = self.convoGetStrings(id, depth+".A")
-        self.strOptionB = self.convoGetStrings(id, depth+".B")
-        if self.intOptionCount > 2:
-            self.strOptionC = self.convoGetStrings(id, depth+".C")
-        else:
-            self.strOptionC = ""
 
-        self.textVisible = ""
-        #self.strOptionA = "I dunno"
-        #self.strOptionB = "Yeah, you got me..."
-        #self.strOptionC = ""
-        self.txtOptionA.setText("")
-        self.txtOptionB.setText("")
-        self.txtOptionC.setText("")
-        self.txtConvo.setText(self.textVisible)
-        #self.intOptionCount = 2
-        self.selectedResponse = 0
-        self.i = 0
-        self.convoResponseSelected = False
-        taskMgr.add(self.gameConvoUpdate, "GameConvoUpdate")
+		self.createPlayer()
 
-    def getIndent(self, value, increment, limit):
-        if (value + increment >= limit):
-            return limit
-        else:
-            return value+increment
+		self.terrainSize = 50
+		self.drawTerrain()
+		self.placeModels()
+		self.placeNPC()
 
-    def setupKeyboard(self):
-        self.accept("arrow_down", self.convoOptionDown)
-        self.accept("arrow_up", self.convoOptionUp)
-        self.accept("enter",self.convoOptionSelect)
+		self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+		self.taskMgr.add(self.step, "GameStep")
 
-    def convoOptionUp(self):
-        self.indent = 0.0
-        if self.selectedResponse == 0:
-            self.selectedResponse = self.intOptionCount-1
-        elif self.selectedResponse > 0:
-            self.selectedResponse -= 1
-
-    def convoOptionDown(self):
-        self.indent = 0.0
-        if self.selectedResponse < self.intOptionCount-1:
-            self.selectedResponse += 1
-        elif self.selectedResponse == self.intOptionCount-1:
-            self.selectedResponse = 0
-
-    def convoOptionSelect(self):
-        self.convoResponseSelected = True
-        if self.selectedResponse == 0:
-            self.convoDepth += ".A.1"
-        elif self.selectedResponse == 1:
-            self.convoDepth += ".B.1"
-        if self.selectedResponse == 2:
-            self.convoDepth += ".C.1"
+		#base.cTrav=CollisionTraverser()
+		#collisionHandler = CollisionHandlerQueue()
 
 
+
+		self.drawUI()
+
+	def createPlayer(self):
+		self.player = self.loader.loadModel("models/man.x") #,{"walk": "models/panda-walk4"}
+		self.playerNode = render.attachNewNode("PlayerNode")
+		self.playerNode.setScale(1, 1, 1)
+		#self.playerCollider = self.player.attachNewNode(CollisionNode('smileycnode'))
+		#self.playerCollider.node().addSolid(CollisionSphere(0, 0, 0, 1))
+		self.player.reparentTo(self.playerNode)
+
+		#self.cTrav.addCollider(self.playerCollider, self.collisionHandler)
+
+		self.playerY = 50
+		self.playerX = 50
+		self.playerZ = 0
+
+		self.playerDir = 0.0
+		self.playerMove = 0.0
+		self.playerTurn = 0.0
+		self.playerJumpHeight = 2
+		self.playerJumpDist = 0
+		self.playerJumpGrav = 0.2
+		self.playerJump = 0
+
+		self.playerSpeed = 6
+		self.playerTurnSpeed = 1
+		self.playerNode.setH(self.playerDir+90)
+
+	def step(self, task):
+		#self.txtStats
+		if (self.playerMove != 0):
+			self.movePlayer(task)
+		if (self.playerTurn != 0):
+			self.turnPlayer(task)
+		if (self.playerJumpDist > 0):
+			self.playerJumpDist += self.playerJump
+			self.playerJump -= self.playerJumpGrav
+		if (self.playerJumpDist <= 1):
+			self.playerJumpDist = 0
+
+		self.drawPlayer()
+		#self.updateStats()
+
+		return Task.cont
+
+	def spinCameraTask(self, task):
+		if (self.playerMove == 0):
+			self.playerAngle = self.playerNode.getH( ) * math.pi / 180.0
+			self.corrAngle = math.pi / 2
+			self.camX = self.playerX+self.cameraDistance*math.cos( self.corrAngle - self.playerAngle )
+			self.camY = self.playerY+self.cameraDistance*-math.sin( self.corrAngle - self.playerAngle )
+
+			self.corrAngle = math.pi / 2
+			self.camX = self.playerX+self.cameraDistance*math.cos( self.corrAngle - self.playerAngle )
+			self.camY = self.playerY+self.cameraDistance*-math.sin( self.corrAngle - self.playerAngle )
+			self.camZ = self.getObjectZ(self.playerX,self.playerY)+25
+
+			self.camera.setPos(self.camX, self.camY, self.camZ)
+			self.camera.setHpr(self.playerDir+270, self.camAngle, 0)
+
+		return Task.cont
+
+	#def updateStats(self):
+		#self.txtStats.setText("playerX: "+str(self.playerX)+" playerY: "+str(self.playerY)+" playerZ: "+str(self.playerZ))
+
+	def drawUI(self):
+		self.imgInv1 = OnscreenImage(image = "textures/inventoryBox.png", pos = (-1, 0, -0.9), scale = (0.1,0.1,0.1))
+		self.imgInv1.setTransparency(TransparencyAttrib.MAlpha)
+		self.imgInv2 = OnscreenImage(image = "textures/inventoryBox.png", pos = (-0.78, 0, -0.9), scale = (0.1,0.1,0.1))
+		self.imgInv2.setTransparency(TransparencyAttrib.MAlpha)
+		self.imgInv3 = OnscreenImage(image = "textures/inventoryBox.png", pos = (-0.56, 0, -0.9), scale = (0.1,0.1,0.1))
+		self.imgInv3.setTransparency(TransparencyAttrib.MAlpha)
+		self.imgInv4 = OnscreenImage(image = "textures/inventoryBox.png", pos = (-0.34, 0, -0.9), scale = (0.1,0.1,0.1))
+		self.imgInv4.setTransparency(TransparencyAttrib.MAlpha)
+		self.imgInv5 = OnscreenImage(image = "textures/inventoryBox.png", pos = (-0.12, 0, -0.9), scale = (0.1,0.1,0.1))
+		self.imgInv5.setTransparency(TransparencyAttrib.MAlpha)
+
+	def getObjectZ(self, x, y):
+		if ((x > 0) and (x < 257) and (y > 0) and (y < 257)):
+			return(self.terrain.getElevation(x,y)*self.terrainSize)
+		else:
+			return 0
+
+	def placeModels(self):
+		cubeCount = 12
+		cubeXInc = 24
+
+		cubeGen = 0
+		cubeGenX = 0
+		cubeGenY = -50
+
+		cubeGenScale = 10
+		cubeGenRot = 90
+
+		while (cubeGen < cubeCount):
+			cube = self.loader.loadModel("models/house2.x")
+			cube.reparentTo(self.render)
+			cube.setScale(cubeGenScale, cubeGenScale*2, cubeGenScale)
+			cube.setPos(0+cubeXInc*cubeGen, cubeGenY, 0)
+
+			cubeGen += 1
+
+		cubeGen = 0
+		while (cubeGen < cubeCount):
+			cubeGen += 1
+
+
+	def placeNPC(self):
+		npcX = -50
+		npcY = -50
+		npcScale = 1
+
+		self.npc = self.loader.loadModel("models/man.x")
+		self.npcNode = render.attachNewNode("PlayerNode")
+		self.npc.reparentTo(self.npcNode)
+		self.npcNode.setScale(1, 1, 1)
+		self.npcNode.setPos(npcX,npcY,0)
+
+		#self.conversationWithNPC()
+
+		#self.npcCollider = frowneyModel.attachNewNode(CollisionNode('npcColNode'))
+		#self.npcCollider.node().addSolid(CollisionSphere(0, 0, 0, 1))
+
+	def conversationWithNPC(self):
+		self.txtConvo.setText("Jim Bob:")
+		self.txtConvoOp1.setText("  - ")
+		self.txtConvoOp2.setText("  - ")
+		self.txtConvoOp3.setText("  - ")
+
+
+	def drawTerrain(self):
+		self.terrain = GeoMipTerrain("terrain")
+		self.terrain.setHeightfield(Filename("textures/heights.png"))
+		self.terrain.setColorMap("textures/texRock2.png")
+
+		self.terrain.setBlockSize(64)
+		self.terrain.setFactor(1)
+		self.terrain.setNear(40)
+		self.terrain.setFar(120)
+		self.terrain.setMinLevel(0)
+		self.terrain.setBruteforce(True)
+		self.terrain.generate()
+		self.terrain.setAutoFlatten(self.terrain.AFMLight)
+		self.terrain.setFocalPoint(Point3(self.playerX,self.playerY,self.playerZ))
+
+		self.root = self.terrain.getRoot()
+		self.root.reparentTo(render)
+		self.root.setSz(self.terrainSize)
+
+	def jumpPlayer(self):
+		if (self.playerJumpDist == 0):
+			self.playerJump = self.playerJumpHeight
+			self.playerJumpDist += self.playerJump
+
+	def movePlayer(self, task):
+		self.playerAngle = self.playerNode.getH( ) * math.pi / 180.0
+		self.corrAngle = math.pi / 2
+
+		self.dx = self.playerMove * math.cos( self.corrAngle - self.playerAngle )
+		self.dy = self.playerMove * -math.sin( self.corrAngle - self.playerAngle )
+
+		self.playerAngle = self.playerNode.getH( ) * math.pi / 180.0
+		self.corrAngle = math.pi / 2
+		self.camX = self.playerX+self.cameraDistance*math.cos( self.corrAngle - self.playerAngle )
+		self.camY = self.playerY+self.cameraDistance*-math.sin( self.corrAngle - self.playerAngle )
+
+		self.playerX += self.dx / 10
+		self.playerY += self.dy / 10
+		self.playerZ = self.getObjectZ(self.playerX,self.playerY)
+		self.camZ = self.getObjectZ(self.playerX,self.playerY)+25
+		self.playerNode.setPos(self.playerX, self.playerY, self.playerZ+self.playerJumpDist)
+		self.camera.setPos(self.camX, self.camY, self.camZ)
+		self.camera.setHpr(self.playerDir+270, self.camAngle, 0)
+
+		self.terrain.setFocalPoint(Point3(self.playerX,self.playerY,self.playerZ))
+		self.terrain.update()
+
+	def drawPlayer(self):
+		self.playerNode.setPos(self.playerX, self.playerY, self.playerZ+self.playerJumpDist)
+
+	def turnPlayer(self, task):
+		self.playerDir += self.playerTurn
+		self.playerNode.setH(self.playerDir+90)
+
+	def keyboardSetup( self ):
+		self.accept("w", self.keyW)
+		self.accept("w-up", self.resetMove)
+		self.accept("s", self.keyS)
+		self.accept("s-up", self.resetMove)
+		self.accept("a", self.keyA)
+		self.accept("enter", self.conversationWithNPC)
+		self.accept("a-up", self.resetDir)
+		self.accept("d", self.keyD)
+		self.accept("d-up", self.resetDir)
+
+		self.accept("space", self.jumpPlayer)
+
+	def keyW( self ):
+		self.playerMove = self.playerSpeed
+
+	def resetMove( self ):
+		self.playerMove = 0
+
+	def resetDir( self ):
+		self.playerTurn = 0
+
+	def keyS( self ):
+		self.playerMove = -self.playerSpeed
+
+	def keyA( self ):
+		self.playerTurn = self.playerTurnSpeed
+
+	def keyD( self ):
+		self.playerTurn = -self.playerTurnSpeed
 
 app = MyApp()
 app.run()
+
+
